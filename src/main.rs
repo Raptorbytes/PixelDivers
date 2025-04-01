8147e653a8453c187138046407d2cd6c56eeadbd
Raptorbytes <PARADOXzss07@raptorbytes.org>
2025-03-31 20:14:59 -0400

    Fixed main.rs error

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
 use serde::{Deserialize, Serialize};
 use std::fs;
-use bevy::prelude::*;
 use std::collections::HashMap;
+use bevy::{prelude::*, app::AppExit, MinimalPlugins, DefaultPlugins};
 fn audio_mute() {
-    App::build()
-        // Disable the audio plugin by not adding it
-        .run();
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Stratagems {
-    stratagems: HashMap<String, Vec<String>>,
+    // App::build()
+    //     // Disable the audio plugin by not adding it
+    //     .run();
+    println!("Audio Muted");
 }
 
 fn load_stratagems() -> Stratagems {
@@ -18,11 +14,15 @@ fn load_stratagems() -> Stratagems {
     serde_json::from_str(&data).expect("JSON format error")
 }
 
-fn setup_stratagems() {
+fn setup_stratagems(mut exit: EventWriter<AppExit>) {
     let stratagems = load_stratagems();
     println!("{:?}", stratagems);
+    exit.send(AppExit);
 }
 
 fn main() {
-    App::new().add_startup_system(setup_stratagems).run();
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_startup_system(setup_stratagems)
+    .run();
 }
